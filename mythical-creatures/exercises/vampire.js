d4b362eca7e682d6db18ba49522163df15560f2f
David Tran <frozenpho@gmail.com>
2021-09-02 08:50:44 -0600

    Complete Vampire exercise

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,22 @@
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    if (pet === undefined) {
+      this.pet = "bat"
+    } else {
+      this.pet = pet;
+    }
+    this.thirsty = true;
+    this.ouncesDrank = 0;
+  }
+  drink() {
+    if (this.ouncesDrank >= 50){
+      return "I\'m too full to drink anymore!";
+    } else if (this.ouncesDrank < 50) {
+      this.thirsty = false;
+      this.ouncesDrank += 10;
+    }
+    }
+  }
+
+module.exports = Vampire;
