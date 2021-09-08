5d2b53bf9c02e03cee46f94501a97df77c1d9986
David Tran <frozenpho@gmail.com>
2021-09-07 21:07:20 -0600

    Complete medusa exercise and associated files

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    if (this.statues.length >= 3){
+      var unStonedPerson = new Person(this.statues[0].name);
+      this.statues.shift();
+      var stonedPerson = new Statue(victim.name)
+      this.statues.push(stonedPerson)
+      unStonedPerson.mood = 'relieved';
+      return unStonedPerson;
+    }
+    var stonedPerson = new Statue(victim.name)
+    this.statues.push(stonedPerson);
+  }
+}
+
+module.exports = Medusa;
